b528cffae5aaf4a8a846934d329c7019a0399086
christian <christian@surlykke.dk>
2012-01-08 12:45:39 +0100

    Added proper copyright statements

--- profiledialog.h
+++ profiledialog.h
@@ -1,19 +1,19 @@
 /*
-  Copyright 2010, 2011 Christian Surlykke
+  Copyright 2011 Christian Surlykke
 
-  This file is part of ProxyManager.
+  This file is part of ProxyManager
 
-  PitchTrainer is free software: you can redistribute it and/or modify
+  ProxyManager is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, version 3
 
-  PitchTrainer is distributed in the hope that it will be useful,
+  ProxyManager is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with PitchTrainer.  If not, see <http://www.gnu.org/licenses/>.
+  along with ProxyManager.  If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef PROFILEMANAGER_H
 #define PROFILEMANAGER_H
--- profilelistmodel.cpp
+++ profilelistmodel.cpp
@@ -1,10 +1,20 @@
 /*
- * profilelistmodel.cpp
- *
- *  Created on: 12/03/2010j
- *      Author: christian
- */
+  Copyright 2011 Christian Surlykke
 
+  This file is part of ProxyManager
+
+  ProxyManager is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, version 3
+
+  ProxyManager is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with ProxyManager.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <QUuid>
 #include "profilelistmodel.h"
 #include <QMessageBox>
